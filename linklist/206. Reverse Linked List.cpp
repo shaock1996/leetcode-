16836d88a94ee4415baa5a5f219272b4467f8d47
shaock1996 <50735757+shaock1996@users.noreply.github.com>
2019-08-19 10:22:49 +0800

    Update 206. Reverse Linked List.cpp

--- linklist/206. Reverse Linked List.cpp
+++ linklist/206. Reverse Linked List.cpp
@@ -1,16 +1,16 @@
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     int val; //值
+ *     ListNode *next; //指向ListNode类型的指针
+ *     ListNode(int x) : val(x), next(NULL) {}  //可用此函数创建并初始化一个链表节点，如ListNode a(10)
  * };
  */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* new_head = NULL;
-        ListNode* next;
+        ListNode* new_head = NULL;   //new_head初始化为NULL,作为翻转后尾结点
+        ListNode* next;              //next指针用于存储head指向的下一个节点
         while(head)
         {
             next = head->next;         
